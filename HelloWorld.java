a295df7b02686b030fba7050d02061abbaada7bc
Pappu Laxman Gaikwad <pappugaikwad595@gmail.com>
2026-02-25 11:19:51 +0530

    new add
    added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HELLO! HELLO!");
+        System.out.println("Hello, World!");
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Nice to meet you, " + name + "!");
+        } else {
+            System.out.println("No name provided!");
+        }
     }
-
 }
