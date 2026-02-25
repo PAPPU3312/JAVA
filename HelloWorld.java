715df3736995d62d1ae749a7543b459353cb06e4
Pappu Laxman Gaikwad <pappugaikwad595@gmail.com>
2026-02-25 11:10:45 +0530

    new
    added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,5 @@
-import java.util.Scanner;
-
 public class HelloWorld {
     public static void main(String[] args) {
-        // Print a greeting
         System.out.println("Hello, World!");
-
-        // Take user input
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        // Personalized output
-        System.out.println("Nice to meet you, " + name + "!");
-        
-        scanner.close();
     }
 }
